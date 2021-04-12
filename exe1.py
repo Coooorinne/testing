2192ca9e80aae8278b7c0c8262f7404d4d4b855c
Corinne Ruckstuhl <corinneruckstuhl@Corinnes-MacBook-Pro.local>
2021-04-12 10:43:51 +0200

    exercise 1

---
+++ exe1.py
@@ -0,0 +1,12 @@
+Input = "101010101" #Insert binary number here
+n =int(Input, 2)    #treats the input as binary
+
+def count_bits(n):
+    x = 0
+    while (n):      #while n == non-zero, cond. = True (e. g. 1), while n == zero, cond. False (e. g. 0)
+        x += n & 1  #x = x + (n&1) --> n AND 1
+        n >>= 1     #right shift by 1
+    return x
+
+
+print(count_bits(n))
