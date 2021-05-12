92a562ddb9e8697335a13dd84a54e3cdd701c3a9
Corinne Ruckstuhl <c_ruckstuhl@hotmail.com>
2021-05-12 12:38:55 +0200

    final code

---
+++ Jewels_and_stones.py
@@ -0,0 +1,43 @@
+#----------------------------------------------------------
+
+# Description
+# You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+#
+# Letters are
+# case sensitive, so "a" is considered a different type of stone from "A".
+#
+# Example
+# Input: jewels = "aA", stones = "aAAbbbb"
+# Output: 3
+
+#------------------------------------------------------------
+jewels = input("Please enter the types of stones that are jewels: ")
+# print(f"type of your response is {type(jewels)}")
+
+stones = input("Please also enter the stones you have: ")
+# print(f"type of your response is {type(stones)}")
+
+
+def jewels_and_stones(jewels, stones):
+
+    count = 0
+
+    for char in jewels:
+        for x in stones:
+            if char == x:
+                count += 1
+
+    return count
+
+
+result = jewels_and_stones(jewels, stones)
+
+print(f"You have {result} jewels")
+
+
+#------------------------------------------------------------
+# Approach
+# 1. Input method
+# 2. Pseudo code for logic
+# 3. Function with code logic
+#------------------------------------------------------------
